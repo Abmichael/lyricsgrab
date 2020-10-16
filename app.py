d93fd86637012f8ab6285a23a38348a9e104d18e
Abraham Michael <abrahammgag@gmail.com>
2020-10-16 13:55:10 +0300

    added additional metadata items

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from grabber import Song
 from sys import argv
 
 
+
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route("/<query>")
 def searcher(query):
 	try:
 		song = Song(query)
-
-	except: song = None
+	except: 
+		song = None
 
 	if song:
-		result = {"song_title":song.full_title,"album_art":song.album_art,"lyrics_url":song.song_url,"Lyrics":song.lyrics}
+		result = {'song_title':song.title,'artist':song.artist,'album':song.album,'album_art':song.album_art,'lyrics_url':song.song_url,'lyrics':song.lyrics}
 		return jsonify(result)
 	else:
 		return "something went wrong, try again!!"
@@ -21,6 +23,7 @@ def searcher(query):
 @app.route("/")
 def home():
 	return "up and running"
+
 if __name__ == '__main__':
 	app.debug=True
 	app.run()
